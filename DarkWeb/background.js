d1fe1f97c8b76152204cb0188627aabaf3497fda
rdm144 <46978172+rdm144@users.noreply.github.com>
2019-04-08 20:29:08 -0500

    Update background.js

--- DarkWeb/background.js
+++ DarkWeb/background.js
@@ -30,16 +30,16 @@ const NOBLUE = "body {filter: sepia(40%); background-color: white; color: black;
 /*
 Add an event listener
 The popup window's event listener broadcasts a message, and this receives it
-Upon receiving a message, it then runs update()
+Upon receiving a message, it then runs updateFilter()
 */
 browser.runtime.onMessage.addListener(updateFilter);
 function updateFilter(recieved, sender, sendResponse) {
-  setCSScode(recieved.message);
-  checkToggle(recieved.message);
+  toggleFilters(recieved.message);
   sendResponse({response: "Response from background.js."});
 }
 
 // This listener is for newly-created tabs
+// After the user switches to the new tab, the code then runs updateNewTab()
 browser.tabs.onUpdated.addListener(updateNewTab); 
 function updateNewTab(recieved, sender, sendResponse) {
   var tabID = browser.tabs.getCurrent().id;
@@ -61,24 +61,25 @@ function setCSScode(buttonID) {
 }
 
 /*
- Compares the current filter to the selected filter
- If they are the same, remove the filter on all tabs
- Else, apply the filter on all tabs
+ Compares the current filter to the selected filter.
+ First removes the previous filter, and then checks if it should apply a new filter
+ If the selected filter is the same as the current filter, then it will just remove it.
+ Else, it will apply the new filter.
 */
-function checkToggle(buttonID) {
+function toggleFilters(buttonID) {
+  removeAllFilters(); // To apply a new filter, we must first remove the old filter
   if(previousID == buttonID) {
-    removeFilter(buttonID);
     previousID = "";
   }
   else {
-    applyFilter(buttonID);
+    setCSScode(buttonID);
     previousID = buttonID;
+    applyFilter();
   }
 }
 
 // Apply the selected filter to all tabs
 function applyFilter() {
-  removeFilter(); // To apply a new filter, we must first remove the old filter.
   var gettingAllTabs = browser.tabs.query({});
   gettingAllTabs.then((tabs) => {
     for (let currentTab of tabs) {
@@ -89,12 +90,17 @@ function applyFilter() {
 }
 
 // Remove the selected filter from all tabs
-function removeFilter() {
+function removeAllFilters() {
+  var cssCodes = [INVERT, GRAYSCALE, SEPIA, NIGHT, ROTATECW, ROTATECCW, NOBLUE];
+
   var gettingAllTabs = browser.tabs.query({});
   gettingAllTabs.then((tabs) => {
     for (let currentTab of tabs) { 
       var tabID = currentTab.id;
-      browser.tabs.removeCSS(tabID, {code: CSS});
+      cssCodes.forEach(function(item) {
+        var code = item; browser.tabs.removeCSS(tabID, {code: code});
+      });
+      //browser.tabs.removeCSS(tabID, {code: CSS});
     }
   });
 }
