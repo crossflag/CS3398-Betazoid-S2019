703d42cb596fd4b51ae63e0f0173a5fd6d9ba1e7
rdm144 <46978172+rdm144@users.noreply.github.com>
2019-04-20 13:50:16 -0500

    Added saveSettings, loadSettings, keyboard shortcut runs loadSettings

--- DarkWeb/background.js
+++ DarkWeb/background.js
@@ -30,32 +30,38 @@ const DROPS = "body {filter: drop-shadow(5px 5px 5px black); background-color: w
 const CONTRAST = "body {filter: contrast(200%); background-color: white; color: black;}";
 const TRANSP = "body {filter: opacity(50%); background-color: white;  color: black;}";
 
+loadSettings(); // Load previously saved color setting on startup
 
 /*
-Load previously saved color setting
+  This handles the keyboard shortcut.
+  The actual keys for the shorcut are defined in manifest.json 
+    under "commands" > "toggle-feature" > "suggested" > default/mac
 */
-toggleFilters(localStorage.getItem('user'));
-
-/*
-Add an event listener
-The popup window's event listener broadcasts a message, and this receives it
-Upon receiving a message, it then runs updateFilter()
-*/
-
 browser.commands.onCommand.addListener(function(command) {
-  if (command == "toggle-feature") {
-    toggleFilters("Invert");
-  }
+  if (command == "toggle-feature")
+    loadSettings(); // Load filter settings upon entering keyboard shortcut
 });
 
+/*
+  Add an event listener.
+  The popup window's event listener broadcasts a message, and this receives it.
+  Upon receiving a message, it then runs updateFilter().
+*/
 browser.runtime.onMessage.addListener(updateFilter);
 function updateFilter(recieved, sender, sendResponse) {
-  toggleFilters(recieved.message);
-  sendResponse({response: "Response from background.js."});
+  if(recieved.message == "SaveSettings")       // user clicked on Save Settings
+    saveSettings();
+  else if(recieved.message == "ApplyPreset")   // user clicked on Apply Settings
+    loadSettings();
+  else
+    toggleFilters(recieved.message);           // user clicked on a specific filter to toggle
+  sendResponse({response: "--- background.js has finished! ---"});
 }
 
-// This listener is for newly-created tabs
-// After the user switches to the new tab, the code then runs updateNewTab()
+/* 
+  This listener is for newly-created tabs.
+  After the user switches to the new tab, the code then runs updateNewTab().
+*/
 browser.tabs.onUpdated.addListener(updateNewTab); 
 function updateNewTab(recieved, sender, sendResponse) {
   var tabID = browser.tabs.getCurrent().id;
@@ -75,24 +81,17 @@ function setCSScode(buttonID) {
     case "Drops":     CSS = DROPS;     break; 
     case "Contrast":  CSS = CONTRAST;  break;
     case "Transp":    CSS = TRANSP;    break;
-    default: break; // Do nothing for default
+    default: console.log("No filter found for this button."); break; // Do nothing for default
   }
 }
 
 /*
- Compares the current filter to the selected filter.
- First removes the previous filter, and then checks if it should apply a new filter
- If the selected filter is the same as the current filter, then it will just remove it.
- Else, it will apply the new filter.
+  Compares the current filter to the selected filter.
+  First removes the previous filter, and then checks if it should apply a new filter
+  If the selected filter is the same as the current filter, then it will just remove it.
+  Else, it will apply the new filter.
 */
 function toggleFilters(buttonID) {
-    if(buttonID == "SaveSettings"){
-        console.log("Saving Settings");
-        var saveID = previousID;
-        localStorage.setItem('user', saveID);
-        console.log(localStorage.getItem('user'));
-        return;
-    }
     removeAllFilters(); // To apply a new filter, we must first remove the all filters
     CSS = ""; // Reset the CSS variable. This fixes tab persistence.
     if(previousID == buttonID) {
@@ -110,8 +109,10 @@ function applyFilter() {
   var gettingAllTabs = browser.tabs.query({});
   gettingAllTabs.then((tabs) => {
     for (let currentTab of tabs) {
-      var tabID = currentTab.id;
-      browser.tabs.insertCSS(tabID, {code: CSS});
+      if(currentTab.url != "about:debugging") {
+        var tabID = currentTab.id;
+        browser.tabs.insertCSS(tabID, {code: CSS});
+      }
     }
   });
 }
@@ -121,11 +122,40 @@ function removeAllFilters() {
   var cssCodes = [INVERT, GRAYSCALE, SEPIA, NIGHT, ROTATECW, ROTATECCW, NOBLUE, DROPS, CONTRAST, TRANSP];
   var gettingAllTabs = browser.tabs.query({});
   gettingAllTabs.then((tabs) => {
-    for (let currentTab of tabs) { 
-      var tabID = currentTab.id;
-      cssCodes.forEach(function(item) {
-        var code = item; browser.tabs.removeCSS(tabID, {code: code});
-      });
+    for (let currentTab of tabs) {
+      if(currentTab.url != "about:debugging") {
+        var tabID = currentTab.id;
+        cssCodes.forEach(function(code) {
+          browser.tabs.removeCSS(tabID, {code: code});
+        });
+      } 
     }
   });
+}
+
+/* 
+  Saves the current filter to the browser's local storage.
+  Currently saves single filters. Must be redesigned for more complex settings in the future.
+*/
+function saveSettings() {
+  console.log("Saving " + previousID + " to local storage.");
+  localStorage.setItem('user', previousID); // Change this later for more complex filters
+}
+
+/*
+  Loads the last saved setting, and applies it to the browser window.
+  Currently applies single filters. Must be redesigned for more complex settings in the future.
+*/
+function loadSettings() {
+  if(localStorage.getItem('user') != null && localStorage.getItem('user') != "") { // validate data before applying
+    console.log("Loading \"" + localStorage.getItem('user') + "\" filter from local storage.");
+
+    // Change this section later for more complex filters
+    removeAllFilters();
+    setCSScode(localStorage.getItem('user')); 
+    previousID = localStorage.getItem('user');
+    applyFilter();
+  }
+  else
+    console.log("No valid data is stored in local storage!");
 }
